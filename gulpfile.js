3e2a65f6df6e033ff94daf7636c2a174609c130d
Antoni Virós Martín <ani300@gmail.com>
2015-11-25 17:01:15 +0100

    Fixes issues 7 and 16

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,47 @@
+var browserify = require('browserify');
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     notify = require('gulp-notify'),
-    bower = require('gulp-bower'),
     postcss = require('gulp-postcss'),
     sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('autoprefixer');
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gutil = require('gulp-util'),
+    autoprefixer = require('autoprefixer'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer');
 
 var config = {
-  sassPath: './sass',
-  bowerDir: './bower_components'
+  sassPath: './sass'
 };
 
-gulp.task('bower', function() {
-  return bower()
-    .pipe(gulp.dest(config.bowerDir));
-});
-
-gulp.task('icons', function() {
-  return gulp.src(config.bowerDir + '/font-awesome/fonts/**.*')
+/*gulp.task('icons', function() {
+  return gulp.src('./fonts/**.*')
     .pipe(gulp.dest('./public/fonts'));
-});
+});*/
 
 gulp.task('js', function() {
-  return gulp.src(config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.min.js')
-    .pipe(gulp.dest('./public/js'));
+  var b = browserify({
+    entries: './js/landing.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('./js/landing.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('css', function() {
   return sass(config.sassPath + '/app.scss', {
         style: 'compressed',
         loadPath: [
-          './sass',
-          config.bowerDir + '/bootstrap-sass/assets/stylesheets',
-          config.bowerDir + '/font-awesome/scss',
+          './sass'
         ]
       })
       .on("error", notify.onError(function (error) {
@@ -48,4 +57,4 @@ gulp.task('watch', function() {
      gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-gulp.task('default', ['bower', 'icons', 'css', 'js']);
+gulp.task('default', ['css', 'js']);
